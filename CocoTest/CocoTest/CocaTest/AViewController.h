b7c933902713da4ff68b70442d7eb8d0df54f2ad
anchanggeng <1443713841@qq.com>
2018-11-14 08:54:04 +0800

    cocotest

---
+++ CocoTest/CocoTest/CocaTest/AViewController.h
@@ -0,0 +1,17 @@
+//
+//  AViewController.h
+//  CocoTest
+//
+//  Created by ACG on 2018/11/14.
+//  Copyright © 2018 ACG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CocoTest/CocoTest/CocaTest/AViewController.m
@@ -0,0 +1,33 @@
+//
+//  AViewController.m
+//  CocoTest
+//
+//  Created by ACG on 2018/11/14.
+//  Copyright © 2018 ACG. All rights reserved.
+//
+
+#import "AViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor=[UIColor yellowColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
